7031a13cc580d3d76729cf6fe2c0102055bb2f95
liyang <235938019@qq.com>
2020-04-18 16:27:37 +0000

    liyang

---
+++ ora.sh
@@ -0,0 +1,55 @@
+# install environment both nodes
+yum -y install oracle-database-preinstall-19c-1.0-1.el7.x86_64.rpm
+
+# install Desktop
+yum -y groups install "GNOME Desktop"
+
+# modify user and group both nodes
+userdel -r oracle
+userdel -r grid
+groupdel oinstall
+groupdel dba
+groupadd -g 5001 oinstall
+groupadd -g 5002 dba
+groupadd -g 5003 asmdba
+groupadd -g 5004 asmoper
+groupadd -g 5005 asmadmin
+useradd -u 6001 -g oinstall -G asmadmin,asmdba,asmoper grid
+useradd -u 6002 -g oinstall -G dba,asmadmin oracle
+
+# create install dir both nodes
+mkdir /opt/oracle
+mkdir -p /opt/oracle/app/grid
+mkdir -p /opt/oracle/app/19c/grid
+chown -R grid:oinstall /opt/oracle
+mkdir -p /opt/oracle/app/oraInventory
+chown -R grid:oinstall /opt/oracle/app/oraInventory
+mkdir -p /opt/oracle/app/oracle/product/19c/dbhome_1
+chown -R oracle:oinstall /opt/oracle/app/oracle
+chmod -R 775 /opt/oracle
+
+# grid env config both nodes
+su - grid
+vi ~/.bash_profile
+umask 022
+export ORACLE_SID=+ASM1
+export ORACLE_BASE=/opt/oracle/app/grid
+export ORACLE_HOME=/opt/oracle/app/19c/grid
+export PATH=.:$PATH:$HOME/bin:$ORACLE_HOME/bin
+
+source ~/.bash_profile
+env |grep ORACLE
+
+# oracle env config both nodes
+su - oracle
+vi ~/.bash_profile
+umask 022
+export ORACLE_BASE=/opt/oracle/app/oracle
+export ORACLE_HOME=$ORACLE_BASE/product/19c/dbhome_1
+export ORACLE_UNQNAME=itpuxdb
+export ORACLE_SID=itpuxdb1
+export NLS_LANG=AMERICAN_AMERICA.ZHS16GBK
+export PATH=.:$PATH:$HOME/bin:$ORACLE_HOME/bin
+
+source ~/.bash_profile
+env |grep ORACLE
---
+++ pcs.sh
@@ -0,0 +1,30 @@
+# nfs-HA with drbd storage
+echo "choise 1 to install pcs on all nodes."
+echo "choise 2 to config pcs cluster only one node."
+read -p "pls input your choise [1]: " n
+
+case $n in
+1)
+yum -y install pcs fence-agents-all lvm2-cluster 
+systemctl enable pcsd
+systemctl start pcsd
+echo "liyang" | passwd --stdin hacluster
+;;
+
+2)
+read -p "pls intput node1 hostname: " pcs1
+read -p "pls intput node2 hostname: " pcs2
+read -p "pls intput cluster name: " name
+pcs cluster auth $pcs1 $pcs2 -u hacluster -p liyang
+pcs cluster setup --name $name $pcs1 $pcs2
+pcs cluster start --all
+pcs cluster enable --all
+pcs property set stonith-enabled=false
+pcs status cluster
+;;
+
+*)
+echo "pls input 1-2 choise."
+exit;
+
+esac
