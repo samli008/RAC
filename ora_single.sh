e2e2f054b02fa8d6bf3084ea1023b01cf89192b1
liyang <235938019@qq.com>
2020-04-23 09:26:31 +0000

    liyang

--- ora_single.sh
+++ ora_single.sh
@@ -20,3 +20,14 @@ sed -i s/ORCLPDB1/pdb1/g oracledb_db1-19c
 sed -i s/AL32UTF8/ZHS16GBK/g oracledb_db1-19c
 
 /etc/init.d/oracledb_db1-19c configure
+
+# config env variable
+cat >> /home/oracle/.bash_profile << EOF
+umask 022
+export ORACLE_BASE=/opt/oracle
+export ORACLE_HOME=\$ORACLE_BASE/product/19c/dbhome_1
+export ORACLE_UNQNAME=oracledb
+export ORACLE_SID=oracledb1
+export NLS_LANG=AMERICAN_AMERICA.ZHS16GBK
+export PATH=.:\$PATH:\$ORACLE_HOME/bin
+EOF
