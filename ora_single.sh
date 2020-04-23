26e75b705ddb613e8c277f569cc1b72a34586a77
liyang <235938019@qq.com>
2020-04-23 08:53:18 +0000

    liyang

--- ora_single.sh
+++ ora_single.sh
@@ -12,3 +12,6 @@ rpm -ivh oracle-database-ee-19c-1.0-1.x86_64.rpm
 cd /etc/sysconfig
 cp oracledb_ORCLCDB-19c.conf oracle_db1-19c.conf
 cat oracle_db1-19c.conf
+
+cd /etc/init.d/
+cp oracledb_ORCLCDB-19c oracledb_db1-19c
