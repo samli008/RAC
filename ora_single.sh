f68438f9baddf35e79c2b72ad400f59664e16a42
liyang <235938019@qq.com>
2020-04-23 08:49:13 +0000

    liyang

--- ora_single.sh
+++ ora_single.sh
@@ -8,3 +8,7 @@ passwd oracle
 chown -R oracle:oinstall /opt/oracle
 chmod -R 755 /opt/oracle
 rpm -ivh oracle-database-ee-19c-1.0-1.x86_64.rpm
+
+cd /etc/sysconfig
+cp oracledb_ORCLCDB-19c.conf oracle_db1-19c.conf
+cat oracle_db1-19c.conf
