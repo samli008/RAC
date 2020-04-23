e635b195fb0c2717f3456de937fdccdb23c81968
liyang <235938019@qq.com>
2020-04-23 09:40:59 +0000

    liyang

--- ora_single.sh
+++ ora_single.sh
@@ -1,14 +1,16 @@
-# prepare env
+# preinstall
 mkdir /opt/oracle
 echo "192.168.6.44 oracle1" >> /etc/hosts
 
 yum -y install oracle-database-preinstall-19c-1.0-1.el7.x86_64.rpm
 passwd oracle
 
+# install oracle software
 chown -R oracle:oinstall /opt/oracle
 chmod -R 755 /opt/oracle
 rpm -ivh oracle-database-ee-19c-1.0-1.x86_64.rpm
 
+# config dbca
 cd /etc/sysconfig
 cp oracledb_ORCLCDB-19c.conf oracledb_db1-19c.conf
 cat oracledb_db1-19c.conf
@@ -19,9 +21,10 @@ sed -i s/ORCLCDB/db1/g oracledb_db1-19c
 sed -i s/ORCLPDB1/pdb1/g oracledb_db1-19c
 sed -i s/AL32UTF8/ZHS16GBK/g oracledb_db1-19c
 
+# dbca
 /etc/init.d/oracledb_db1-19c configure
 
-# config env variable
+# config oracle user env variable
 cat >> /home/oracle/.bash_profile << EOF
 umask 022
 export ORACLE_BASE=/opt/oracle
@@ -37,3 +40,4 @@ EOF
 /etc/init.d/oracledb_db1-19c start
 su - oracle
 lsnrctl status
+ps -ef |grep smon
