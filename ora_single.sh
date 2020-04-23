e39504c14cf0fc3b5d7e37589395ab4e607f2351
liyang <235938019@qq.com>
2020-04-23 04:49:30 +0000

    liyang

--- ora_single.sh
+++ ora_single.sh
@@ -1,7 +1,10 @@
 # prepare env
 mkdir /opt/oracle
 echo "192.168.6.44 oracle1" >> /etc/hosts
+
 yum -y install oracle-database-preinstall-19c-1.0-1.el7.x86_64.rpm
 passwd oracle
+
 chown -R oracle:oinstall /opt/oracle
 chmod -R 755 /opt/oracle
+rpm -ivh oracle-database-ee-19c-1.0-1.x86_64.rpm
