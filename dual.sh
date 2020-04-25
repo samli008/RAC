0ee0f3ebf724545e4458173cd1d14d0d54581575
liyang <235938019@qq.com>
2020-04-25 03:27:51 +0000

    liyang

--- dual.sh
+++
@@ -1,7 +0,0 @@
-read -p "pls input drbd resource [vdb]: " drbd
-pcs cluster cib drbd_cfg
-pcs -f drbd_cfg resource create $drbd ocf:linbit:drbd drbd_resource=$drbd op monitor interval=60s
-pcs -f drbd_cfg resource master clone$drbd $drbd master-max=2 master-node-max=1 clone-max=2 clone-node-max=1 notify=true
-pcs -f drbd_cfg resource show
-pcs cluster cib-push drbd_cfg
-drbdadm status $drbd
